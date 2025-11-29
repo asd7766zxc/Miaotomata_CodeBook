36652249f73385fee8c1a433d75e02045534fa7d
GUaN <152492391+GU7432@users.noreply.github.com>
2025-11-30 00:19:10 +0800

    重構 exgcd 函數，將整數類型改為 i64，並新增 inverse 函數以計算模逆元

--- math/exgcd.cpp
+++ math/exgcd.cpp
@@ -1,6 +1,14 @@
-int exgcd(int a,int b,int&x,int&y){
-    if(b==0)return x=1,y=0,a;
-    int d = exgcd(b,a%b,y,x);
-    y-=a/b*x;
-    return d;
-}
+i64 exgcd(i64 a, i64 b, i64 &x, i64 &y) {
+    if (b == 0) return x = 1, y = 0, a;
+    i64 g = exgcd(b, a % b, y, x);
+    y -= a / b * x;
+    return g;
+} // ax + by = gcd(a, b) /* SPLIT-HASH */
+i64 inverse(i64 a, i64 b, i64 m) {
+    auto g = gcd(m, a);
+    if (b % g) return -1;
+    i64 x, y; exgcd(a/g, m/g, x, y);
+    auto r = x * (b/g) % m;
+    if (r < 0) r += m;
+    return r % (m/g);
+} // ar ≡ b % m
